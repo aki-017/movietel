8cade7b2fe54c2855b49a8e8863833b7da49ec4b
aki <aki@aki017.info>
2012-11-09 19:08:37 +0900

    インデント戦争

--- server.c
+++ server.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,81 +11,81 @@
 #define BUFFER_SIZE (100)
 
 int main(int argc, char** argv){
-  struct sockaddr_in server, client;
-  char buf[BUFFER_SIZE+1];
-  int s, new_s;
-  int pid, nbyte;
-  int cl_len;
-  int yes = 1;
+    struct sockaddr_in server, client;
+    char buf[BUFFER_SIZE+1];
+    int s, new_s;
+    int pid, nbyte;
+    int cl_len;
+    int yes = 1;
 
-  if(argc < 2){
-    fprintf(stderr, "Usage : %s port_number\n", argv[0]);
-    exit(1);
-  }
-
-  if((s = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-    perror("socket");
-    exit(EXIT_FAILURE);
-  }
-
-  bzero((char*)&server, sizeof(server));
-  server.sin_family = AF_INET;
-  server.sin_addr.s_addr = INADDR_ANY;
-  server.sin_port = htons(atoi(argv[1]));
-
-  setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const char*)&yes, sizeof(yes));
-
-  if(bind(s, (struct sockaddr*)&server, sizeof(server)) == -1){
-    perror("bind");
-    exit(EXIT_FAILURE);
-  }
-
-  if(listen(s, BACKLOG) == -1){
-    perror("listen");
-    exit(EXIT_FAILURE);
-  }
-
-  for(;;){
-    cl_len = sizeof(client);
-    if((new_s = accept(s, (struct sockaddr*)&client, &cl_len)) == -1){
-      perror("accept");
-      exit(EXIT_FAILURE);
+    if(argc < 2){
+        fprintf(stderr, "Usage : %s port_number\n", argv[0]);
+        exit(1);
     }
 
-    pid = fork();
+    if((s = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
-    switch(pid){
-    case -1:
-      perror("fork");
-      exit(EXIT_FAILURE);
-      break;
-    case 0:
-      // child process
-      close(s);
-      printf("fork success\n");
+    bzero((char*)&server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(atoi(argv[1]));
 
-      {
-        FILE* in_pipe = popen("./moviecat", "r");
-        if(in_pipe != NULL){
-          nbyte = fread(buf, sizeof(char), BUFFER_SIZE, in_pipe);
-          printf("nbyte : %d\n", nbyte);
-          while(nbyte > 0){
-            write(new_s, &buf, nbyte);
-            nbyte = fread(buf, sizeof(char), BUFFER_SIZE, in_pipe);
-            printf("while nbyte : %d", nbyte);
-          }
-          exit(EXIT_SUCCESS);
-        }else{
-          perror("popen: ");
-          exit(EXIT_FAILURE);
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const char*)&yes, sizeof(yes));
+
+    if(bind(s, (struct sockaddr*)&server, sizeof(server)) == -1){
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(s, BACKLOG) == -1){
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    for(;;){
+        cl_len = sizeof(client);
+        if((new_s = accept(s, (struct sockaddr*)&client, &cl_len)) == -1){
+            perror("accept");
+            exit(EXIT_FAILURE);
         }
-      }
-      break;
 
-    default:
-      //parent process
-      close(new_s);
+        pid = fork();
+
+        switch(pid){
+            case -1:
+                perror("fork");
+                exit(EXIT_FAILURE);
+                break;
+            case 0:
+                // child process
+                close(s);
+                printf("fork success\n");
+
+                {
+                    FILE* in_pipe = popen("./moviecat ../moviecat/movie.mp4", "r");
+                    if(in_pipe != NULL){
+                        nbyte = fread(buf, sizeof(char), BUFFER_SIZE, in_pipe);
+                        //          printf("nbyte : %d\n", nbyte);
+                        while(nbyte > 0){
+                            write(new_s, &buf, nbyte);
+                            nbyte = fread(buf, sizeof(char), BUFFER_SIZE, in_pipe);
+                            //            printf("while nbyte : %d", nbyte);
+                        }
+                        exit(EXIT_SUCCESS);
+                    }else{
+                        perror("popen: ");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                break;
+
+            default:
+                //parent process
+                close(new_s);
+        }
     }
-  }
-  return 0;
+    return 0;
 }
