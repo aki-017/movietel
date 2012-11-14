e5060eebef76520b1cec20780a2e06c51cde6107
aki <aki@aki017.info>
2012-11-14 18:21:36 +0900

    Shellっぽいの追加

--- movietel.c
+++ movietel.c
@@ -1,17 +1,19 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include <sys/stat.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #define READ_BUFFER_SIZE (1024)
 
+void execute(char *s);
+void cat(char *s);
 int check_file_exist(char* target){
   struct stat sb;
   if(stat(target, &sb) == -1){
@@ -27,6 +29,7 @@ int main(int argc, char** argv){
   int server_socket;
   int client_socket;
   int yes = 1;
+  int len ;
 
   struct sockaddr_in client;
   socklen_t client_len = sizeof(client);
@@ -35,7 +38,9 @@ int main(int argc, char** argv){
   size_t read_size;
   size_t send_size;
 
-  char read_buffer[READ_BUFFER_SIZE];
+  char read_buffer[READ_BUFFER_SIZE] = {'\0'};
+  char shell_buffer[READ_BUFFER_SIZE] = {'\0'};
+  char socket_read_buffer[READ_BUFFER_SIZE] = {'\0'};
 
   // argument check
   if(argc < 2){
@@ -100,6 +105,23 @@ int main(int argc, char** argv){
       close(server_socket);
       printf("accepted connection from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 
+      while((len=recv( client_socket, socket_read_buffer,  sizeof(char), 0)) != -1){
+        if((socket_read_buffer[0] == 10) || (socket_read_buffer[0] == 13)){
+          execute(shell_buffer);
+          sprintf(read_buffer, "guest@MOVIETEL $ %s\n", shell_buffer);
+          send_size = send(client_socket, read_buffer, strlen(read_buffer)*sizeof(char), 0);
+          if(send_size == -1 || send_size == 0){
+            // send error (usually connection close)
+            printf("connection close %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+            exit(EXIT_SUCCESS);
+          }
+          shell_buffer[0]='\0';
+        }else{
+          strcat(shell_buffer, socket_read_buffer);
+        }
+
+      }
+
       movie_pipe = popen("./moviecat ./movie.mp4", "r");
       if(movie_pipe != NULL){
         read_size = fread(read_buffer, sizeof(char), READ_BUFFER_SIZE, movie_pipe);
@@ -124,3 +146,44 @@ int main(int argc, char** argv){
   }
   return 0;
 }
+
+void execute(char *s){
+  char *words[10];
+  int i;
+  const char* dlm = " ";
+
+  printf("> %s \n", s);
+  for (i = 0; i < 10; i++) {
+    if ((words[i] = strtok(s, dlm)) == NULL)
+      break;
+    s = NULL;
+    printf("arg[%d]=%s \n", i, words[i]);
+  }
+  if (strcmp(words[0] , "exit") == 0) exit(EXIT_SUCCESS);
+  if (strcmp(words[0] , "help") == 0) cat("help.txt");
+  if (strcmp(words[0] , "list") == 0) cat("list.txt");
+}
+
+void cat(char* filename){
+  FILE *fp;
+  char s[256];
+  // exist test
+  if(check_file_exist(filename) != 0){
+    fprintf(stderr, "%s file not found", filename);
+    exit(EXIT_FAILURE);
+  }
+
+
+  if ((fp = fopen(filename,  "r")) == NULL) {
+    printf("file open error!!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (fgets(s,  256,  fp) != NULL) {
+    printf("%s",  s);
+  }
+  fclose(fp);
+
+  return;
+}
+
